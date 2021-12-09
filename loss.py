6d2ae124df7fe1e171c047ccce35c33f9d23da71
Paridhi Maheshwari <1997.paridhi@gmail.com>
2021-12-08 20:56:07 -0800

    documentation for losses

--- loss.py
+++ loss.py
@@ -6,7 +6,9 @@ import torch.nn.functional as F
 
 class infonce(nn.Module):
 	"""
-	The InfoNCE (NT-XENT) loss in contrastive learning.
+	The InfoNCE (NT-XENT) loss in contrastive learning. The implementation
+	follows the paper `A Simple Framework for Contrastive Learning of 
+	Visual Representations <https://arxiv.org/abs/2002.05709>`.
 	"""
 
 	def __init__(self):
@@ -18,12 +20,12 @@ class infonce(nn.Module):
 	def forward(self, embed_anchor, embed_positive):
 		"""
 		Args:
-			embed_anchor, embed_positive: Tensor of shape [batch_size, z_dim]
+			embed_anchor, embed_positive: Tensor of shape [batch_size, embed_dim]
 			tau: Float. Usually in (0,1].
 			norm: Boolean. Whether to apply normlization.
 		"""
 
-		batch_size, _ = embed_anchor.size()
+		batch_size = embed_anchor.shape[0]
 		sim_matrix = torch.einsum("ik,jk->ij", embed_anchor, embed_positive)
 
 		if self.norm:
@@ -59,6 +61,7 @@ class jensen_shannon(nn.Module):
 			positive (bool): Set True if the d_prime is masked for positive pairs,
 							set False for negative pairs.
 		"""
+
 		log_2 = np.log(2.)
 		if positive:
 			score = log_2 - F.softplus(-masked_d_prime)
@@ -71,19 +74,20 @@ class jensen_shannon(nn.Module):
 		Args:
 			embed_anchor, embed_positive: Tensor of shape [batch_size, embed_dim].
 		"""
-		device = embed_anchor.device
-		num_graphs = embed_anchor.shape[0]
 
-		pos_mask = torch.zeros((num_graphs, num_graphs)).to(device)
-		neg_mask = torch.ones((num_graphs, num_graphs)).to(device)
-		for graphidx in range(num_graphs):
+		device = embed_anchor.device
+		batch_size = embed_anchor.shape[0]
+
+		pos_mask = torch.zeros((batch_size, batch_size)).to(device)
+		neg_mask = torch.ones((batch_size, batch_size)).to(device)
+		for graphidx in range(batch_size):
 			pos_mask[graphidx][graphidx] = 1.
 			neg_mask[graphidx][graphidx] = 0.
 
 		d_prime = torch.matmul(embed_anchor, embed_positive.t())
 
 		E_pos = self.get_expectation(d_prime * pos_mask, positive=True).sum()
-		E_pos = E_pos / num_graphs
+		E_pos = E_pos / batch_size
 		E_neg = self.get_expectation(d_prime * neg_mask, positive=False).sum()
-		E_neg = E_neg / (num_graphs * (num_graphs - 1))
+		E_neg = E_neg / (batch_size * (batch_size - 1))
 		return E_neg - E_pos
